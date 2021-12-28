384bab8d9f3e25071d90fc60725164941a250f3b
claitonbarreto <claitonbarreto@gmaiil.com>
2021-12-28 11:30:49 -0300

    test: create first teste scripts

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,53 @@
+const MiniExpress = require('../index')();
+
+describe('App Tests', () => {
+
+    it('should create a app instance', () => {
+    
+        const app = MiniExpress.createApp();
+    
+        expect(app).toBeDefined();
+        expect(app.use).toBeDefined();
+        expect(app.listen).toBeDefined();
+        expect(app.Router).toBeDefined();
+    })
+
+    it('should create a app instance with declared route', () => {
+        
+        const app = MiniExpress.createApp();
+        const router = new MiniExpress.Router();
+
+        router.get('/', (req, res) => {
+            res.json({
+                message: 'Hello World'
+            })
+        })
+
+        app.use(router);
+        app.listen(3000);
+        expect(app).toBeDefined();
+    })
+
+    it('should create a app instance with declared route and middleware', () => {
+            
+        const app = MiniExpress.createApp();
+        const router = new MiniExpress.Router();
+
+        router.get('/', (req,res,next) => {
+            return next();
+        }, (req, res) => {
+            res.json({
+                message: 'Hello World'
+            })
+        })
+
+        app.use(router, {
+            debug: true
+        });
+        expect(app).toBeDefined();
+    })
+
+})
+
+
+
